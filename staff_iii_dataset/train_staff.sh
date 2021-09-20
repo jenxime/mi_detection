d8f5873a6ad3983a264281edf7ffe5b2ecf47aeb
jenxime <67129857+jenxime@users.noreply.github.com>
2021-09-20 08:54:10 -0500

    Cambio rutas

--- staff_iii_dataset/train_staff.sh
+++ staff_iii_dataset/train_staff.sh
@@ -10,16 +10,16 @@ for ((channel=0; channel<9; channel+=1)); do
     
     # gpu0
     for seed in 1 2; do
-        screen -d -m -S chan${channel}_seed$seed bash -c "source /home/arjung2/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=2"
+        screen -d -m -S chan${channel}_seed$seed bash -c "source /home/jenxime/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python /content/mi_detection/staff_iii_dataset/train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=2"
     done
  
     # gpu1
     seed=3
-    screen -d -m -S chan${channel}_seed$seed bash -c "source /home/arjung2/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=0"
+    screen -d -m -S chan${channel}_seed$seed bash -c "source /home/jenxime/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python /content/mi_detection/staff_iii_dataset/train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=0"
 
     # gpu2
     for seed in 4 5 6; do
-        screen -d -m -S chan${channel}_seed$seed bash -c "source /home/arjung2/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=1"
+        screen -d -m -S chan${channel}_seed$seed bash -c "source /home/jenxime/project/anaconda3/etc/profile.d/conda.sh && conda activate py27 && echo \"$channel\" \"$seed\" && python /content/mi_detection/staff_iii_dataset/train_staff.py --channel=\"$channel\" --seed=\"$seed\" --gpu=1"
     done
 
 done
